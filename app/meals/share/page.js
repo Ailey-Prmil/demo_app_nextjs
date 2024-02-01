c02fff8e61722e42e5e1329787bf9e1d8ad44cdd
S0lux <loctranphuoc456@protonmail.com>
2024-02-01 19:46:22 +0700

    Fixed image preview not working

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,11 +1,22 @@
 "use client";
-import ImagePicker, { ImagePreview } from '@/components/meals/imagePicker';
-import classes from './page.module.css';
-import { useState, useEffect } from 'react';
+import ImagePicker, { ImagePreview } from "@/components/meals/imagePicker";
+import classes from "./page.module.css";
+import { useState, useEffect } from "react";
 
 export default function ShareMealPage() {
   const [pickedImage, setPickedImage] = useState();
   const [previewUrl, setPreviewUrl] = useState("");
+
+  useEffect(() => {
+    if (pickedImage) {
+      const fileReader = new FileReader();
+
+      fileReader.readAsDataURL(pickedImage);
+      fileReader.onloadend = () => {
+        setPreviewUrl(fileReader.result);
+      };
+    }
+  }, [pickedImage]);
 
   return (
     <>
@@ -16,41 +27,42 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-            
-            <form className={classes.form}>
-              <div className={classes.row}>
-                <p>
-                  <label htmlFor="name">Your name</label>
-                  <input type="text" id="name" name="name" required />
-                </p>
-                <p>
-                  <label htmlFor="email">Your email</label>
-                  <input type="email" id="email" name="email" required />
-                </p>
-              </div>
-              <p>
-                <label htmlFor="title">Title</label>
-                <input type="text" id="title" name="title" required />
-              </p>
-              <p>
-                <label htmlFor="summary">Short Summary</label>
-                <input type="text" id="summary" name="summary" required />
-              </p>
-              <p>
-                <label htmlFor="instructions">Instructions</label>
-                <textarea
-                  id="instructions"
-                  name="instructions"
-                  rows="10"
-                  required
-                ></textarea>
-              </p>
-              <ImagePicker onImageChange={(e) => setPickedImage(e.currentTarget.files[0])} />
-              <p className={classes.actions}>
-                <button type="submit">Share Meal</button>
-              </p>
-            </form>
-            <ImagePreview image={pickedImage} className=" grid-cols-2"/>
+        <form className={classes.form}>
+          <div className={classes.row}>
+            <p>
+              <label htmlFor="name">Your name</label>
+              <input type="text" id="name" name="name" required />
+            </p>
+            <p>
+              <label htmlFor="email">Your email</label>
+              <input type="email" id="email" name="email" required />
+            </p>
+          </div>
+          <p>
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" required />
+          </p>
+          <p>
+            <label htmlFor="summary">Short Summary</label>
+            <input type="text" id="summary" name="summary" required />
+          </p>
+          <p>
+            <label htmlFor="instructions">Instructions</label>
+            <textarea
+              id="instructions"
+              name="instructions"
+              rows="10"
+              required
+            ></textarea>
+          </p>
+          <ImagePicker
+            onImgChange={(e) => setPickedImage(e.currentTarget.files[0])}
+          />
+          <p className={classes.actions}>
+            <button type="submit">Share Meal</button>
+          </p>
+        </form>
+        <ImagePreview image={previewUrl} className=" grid-cols-2" />
       </main>
     </>
   );
